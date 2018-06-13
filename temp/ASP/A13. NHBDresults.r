12b050c7beab7a6f99cf9ba123e72ef1e44a3769
anasanz <anasanz.asp@gmail.com>
2018-06-13 14:58:11 +0200

    Results whole population coefficients

--- temp/ASP/A13. NHBDresults.r
+++ temp/ASP/A13. NHBDresults.r
@@ -10452,7 +10452,7 @@ c <- palette(c(cc,"purple","brown"))
 c[c=="yellow"] <- "orange"
 # rescale dist
 par(mfrow=c(1,2))
-plot(-30,ylim = c(-7,15), xlim=c(0,34),
+plot(-30,ylim = c(-7,10), xlim=c(0,34),
      pch = 21, col = c[r$Method],bg=adjustcolor(c[r$Method],alpha.f = 0.5) , ylab = "NHBD", xlab = " ", axes = FALSE, main = "Males")
 axis(1,lwd.tick=0, labels = FALSE)
 axis(1,at=c(11,22),labels=c("",""))
@@ -10466,10 +10466,6 @@ points(r$NHBD,
 
 abline(h=0, lty=2)
 
-legend(x=23,y=20, legend = unique(r$Method)[1:5], col = c[r$Method][1:5],
-       pch = 18, cex = 0.85,bty="o",bg=grey(0.9),box.col=grey(0.9) )
-legend(x=28,y=20, legend = unique(r$Method)[6:10], col = c[r$Method][6:10],
-       pch = 18, cex = 0.85,bty="o",bg=grey(0.9),box.col=grey(0.9)  )
 
 # For 8C and 9C cluster divisions, there is no individuals that establishes in the
 # same cluster for long dispersing males (There is no E = 1 in the Category 1)
@@ -10495,7 +10491,7 @@ c <- palette(c(cc,"purple","brown"))
 c[c=="yellow"] <- "orange"
 # rescale dist
 
-plot(-30,ylim = c(-7,15), xlim=c(0,34),
+plot(-30,ylim = c(-7,10), xlim=c(0,34),
      pch = 21, col = c[r$Method],bg=adjustcolor(c[r$Method],alpha.f = 0.5) , ylab = "NHBD", xlab = " ", axes = FALSE, main = "Females")
 axis(1,lwd.tick=0, labels = FALSE)
 axis(1,at=c(11,22),labels=c("",""))
@@ -10509,9 +10505,9 @@ points(r$NHBD,
 
 abline(h=0, lty=2)
 
-legend(x=23,y=15, legend = unique(r$Method)[1:5], col = c[r$Method][1:5],
+legend(x=19,y=10, legend = unique(r$Method)[1:5], col = c[r$Method][1:5],
        pch = 18, cex = 0.85,bty="o",bg=grey(0.9),box.col=grey(0.9) )
-legend(x=28,y=15, legend = unique(r$Method)[6:10], col = c[r$Method][6:10],
+legend(x=30,y=10, legend = unique(r$Method)[6:10], col = c[r$Method][6:10],
        pch = 18, cex = 0.85,bty="o",bg=grey(0.9),box.col=grey(0.9)  )
 
 
@@ -10536,3 +10532,165 @@ axis(2)
 
 legend(x=2.5,y=80, legend = unique(av$Sex), col = c("blue","red"),
        pch = 18, cex = 0.85,bty="o",bg=grey(0.9),box.col=grey(0.9) )
+
+# 3---- General results whole population(Coefficients env covariates and wolf density effect)----
+setwd("C:/Users/Ana/Desktop/MASTER THESIS/Publication/Datos")
+e <- read.csv("Data_NHBD_id_wolf_density_distances.csv")
+e <- e[ ,-which(colnames(e) %in% c("humanlands_1","agri_1", "dem_1", "roughness_1", "mires_1", "roadens_sec1"))]
+
+#CLUSTER 1
+cluster.1<-e[e$Clusters == 1, ]
+length(which(cluster.1$Category == "Natal"))
+natal<-cluster.1[cluster.1$Category == "Natal", ]
+#Select only the IDS that are born in this cluster (natal territories + their available and established)
+cl1 <- cluster.1[which(cluster.1$ID_individual %in% natal$ID_individual), ]
+#For the IDs born in cl, where did they establish and what did they have available?
+b1<-e[which(e$ID_individual %in% cl1$ID_individual), ]
+
+#Transform category to binary variable: CASE-CONTROL
+b1$Category <- as.character(b1$Category)
+b1<-b1[!b1$Category == "Natal", ]
+b1$Category[b1$Category == "Established"] <- 1
+b1$Category[b1$Category == "Available"] <- 0
+b1$Category<-as.numeric(b1$Category)
+#Cluster as factor
+b1$Clusters <- as.factor(b1$Clusters)
+#Create E
+b1$E<-b1$Category
+b1$E <- 0
+b1$E[b1$Clusters == 1] <- 1
+
+
+#CLUSTER 2
+cluster.2<-e[e$Clusters == 2, ]
+length(which(cluster.2$Category == "Natal"))
+natal<-cluster.2[cluster.2$Category == "Natal", ]
+#Select only the IDS that are born in this cluster
+cl2 <- cluster.2[which(cluster.2$ID_individual %in% natal$ID_individual), ]
+#For the IDs born in cl, where did they establish and what did they have available?
+b2<-e[which(e$ID_individual %in% cl2$ID_individual), ]
+
+#Transform category to binary variable: CASE-CONTROL
+b2$Category <- as.character(b2$Category)
+b2<-b2[!b2$Category == "Natal", ]
+b2$Category[b2$Category == "Established"] <- 1
+b2$Category[b2$Category == "Available"] <- 0
+b2$Category<-as.numeric(b2$Category)
+#Cluster as factor
+b2$Clusters <- as.factor(b2$Clusters)
+#Create E
+b2$E<-b2$Category
+b2$E <- 0
+b2$E[b2$Clusters == 2] <- 1
+unique(b2$ID_individual)
+
+
+#CLUSTER 3
+
+cluster.3<-e[e$Clusters == 3, ]
+length(which(cluster.3$Category == "Natal"))
+natal<-cluster.3[cluster.3$Category == "Natal", ]
+#Select only the IDS that are born in this cluster
+cl3 <- cluster.3[which(cluster.3$ID_individual %in% natal$ID_individual), ]
+#For the IDs born in cl, where did they establish and what did they have available?
+b3<-e[which(e$ID_individual %in% cl3$ID_individual), ]
+
+#Transform category to binary variable: CASE-CONTROL
+b3$Category <- as.character(b3$Category)
+b3<-b3[!b3$Category == "Natal", ]
+b3$Category[b3$Category == "Established"] <- 1
+b3$Category[b3$Category == "Available"] <- 0
+b3$Category<-as.numeric(b3$Category)
+#Cluster as factor
+b3$Clusters <- as.factor(b3$Clusters)
+#Create E
+b3$E<-b3$Category
+b3$E <- 0
+b3$E[b3$Clusters == 3] <- 1
+unique(b3$ID_individual)
+
+
+#CLUSTER 4
+cluster.4<-e[e$Clusters == 4, ]
+length(which(cluster.4$Category == "Natal"))
+natal<-cluster.4[cluster.4$Category == "Natal", ]
+#Select only the IDS that are born in this cluster
+cl4 <- cluster.4[which(cluster.4$ID_individual %in% natal$ID_individual), ]
+#For the IDs born in cl, where did they establish and what did they have available?
+b4<-e[which(e$ID_individual %in% cl4$ID_individual), ]
+
+#Transform category to binary variable: CASE-CONTROL
+b4$Category <- as.character(b4$Category)
+b4<-b4[!b4$Category == "Natal", ]
+b4$Category[b4$Category == "Established"] <- 1
+b4$Category[b4$Category == "Available"] <- 0
+b4$Category<-as.numeric(b4$Category)
+#Cluster as factor
+b4$Clusters <- as.factor(b4$Clusters)
+#Create E
+b4$E<-b4$Category
+b4$E <- 0
+b4$E[b4$Clusters == 4] <- 1
+unique(b4$ID_individual)
+
+#CLUSTER 5
+
+cluster.5<-e[e$Clusters == 5, ]
+length(which(cluster.5$Category == "Natal"))
+natal<-cluster.5[cluster.5$Category == "Natal", ]
+#Select only the IDS that are born in this cluster
+cl5 <- cluster.5[which(cluster.5$ID_individual %in% natal$ID_individual), ]
+#For the IDs born in cl, where did they establish and what did they have available?
+b5<-e[which(e$ID_individual %in% cl5$ID_individual), ]
+
+#Transform category to binary variable: CASE-CONTROL
+b5$Category <- as.character(b5$Category)
+b5<-b5[!b5$Category == "Natal", ]
+b5$Category[b5$Category == "Established"] <- 1
+b5$Category[b5$Category == "Available"] <- 0
+b5$Category<-as.numeric(b5$Category)
+#Cluster as factor
+b5$Clusters <- as.factor(b5$Clusters)
+#Create E
+b5$E<-b5$Category
+b5$E <- 0
+b5$E[b5$Clusters == 5] <- 1
+unique(b5$ID_individual)
+
+
+#CLUSTER 6
+
+cluster.6<-e[e$Clusters == 6, ]
+length(which(cluster.6$Category == "Natal"))
+natal<-cluster.6[cluster.6$Category == "Natal", ]
+#Select only the IDS that are born in this cluster
+cl6 <- cluster.6[which(cluster.6$ID_individual %in% natal$ID_individual), ]
+#For the IDs born in cl, where did they establish and what did they have available?
+b6<-e[which(e$ID_individual %in% cl6$ID_individual), ]
+
+#Transform category to binary variable: CASE-CONTROL
+b6$Category <- as.character(b6$Category)
+b6<-b6[!b6$Category == "Natal", ]
+b6$Category[b6$Category == "Established"] <- 1
+b6$Category[b6$Category == "Available"] <- 0
+b6$Category<-as.numeric(b6$Category)
+#Cluster as factor
+b6$Clusters <- as.factor(b6$Clusters)
+#Create E
+b6$E<-b6$Category
+b6$E <- 0
+b6$E[b6$Clusters == 6] <- 1
+unique(b6$ID_individual)
+
+
+#JOIN DATASETS
+clr <- rbind( b1,b2,b3,b4,b5,b6)
+
+c <- clogit(Category ~ E  + wolf_density * E  + Sex * E				
+            + human_1 + forest_1 + water_1				
+            + mountains_1 + mainroad_1 + bear_1 				
+            + slope_1 + roadbuild_1 + moose_dens 				
+            + strata(ID_individual), clr)	
+summary(c)
+confint(c,level = 0.95)
+
